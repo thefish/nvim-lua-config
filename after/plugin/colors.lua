13f8c8afb7710a57fde5a043593118ede0ae38c3
thefish <thefish@zaar.be>
2024-04-13 00:19:57 +0300

    rn color fn

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,4 @@
-function ColorMyPencils(color)
+function SetColor(color)
     color = color or "rose-pine-moon"
     vim.cmd.colorscheme(color)
 
@@ -7,4 +7,4 @@ function ColorMyPencils(color)
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils()
+SetColor()
