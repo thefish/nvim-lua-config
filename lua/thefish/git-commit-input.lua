25a01734f6cdd40bd9188ed053a03658a56c22a6
thefish <thefish@zaar.be>
2024-04-13 22:44:22 +0300

    added better callback for vim.ui.input

--- lua/thefish/git-commit-input.lua
+++ lua/thefish/git-commit-input.lua
@@ -5,15 +5,32 @@ local gcselect = function()
     }, function(selected) end)
 end;
 
-
+local input
 local gcinput = function ()
     vim.ui.input({
         prompt = "Commit msg",
         telescope = require("telescope.themes").get_cursor(),
-    }, function(input) end)
+    }, function(str) input = str end)
 end
 
+
+local function run_input(opts)
+  opts = opts or {}
+  local tx, rx = channel.oneshot()
+  vim.ui.input(opts, tx)
+  if opts.after_fn then
+    opts.after_fn()
+  end
+  return rx()
+end
+
+
 return function()
-    local msg = gcinput()
-    vim.fn.system(string.format('git commit -am "%s"', msg))
+    -- local msg = run_input({prompt = "commit msg"})
+    vim.ui.input({prompt="git commit msg"}, function (msg)
+        -- print("trololo")
+        vim.fn.system(string.format('git commit -am "%s"', msg))
+        -- print(vim.inspect(string.format('git commit -am "%s"', msg)))
+        -- print('ololo')
+    end)
 end
