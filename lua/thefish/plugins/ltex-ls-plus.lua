aa36bd1700a0831cef7bd105f1eafc156ac61b3b
thefish <thefish@zaar.be>
2026-05-11 08:16:43 +0300

    add ltex-plus

---
+++ lua/thefish/plugins/ltex-ls-plus.lua
@@ -0,0 +1,28 @@
+return {
+	-- install  https://github.com/ltex-plus/ltex-ls-plus via mason
+	"barreiroleo/ltex_extra.nvim",
+	dependencies = { "neovim/nvim-lspconfig" },
+
+	opts = {
+		load_langs = { "en-US", "ru-RU" },
+		-- save to .ltex dir
+		path = ".ltex",
+	},
+
+	config = function(_, opts)
+		-- Ltex LSP
+		vim.lsp.config("ltex_plus", {
+			on_attach = function()
+				require("ltex_extra").setup(opts)
+			end,
+
+			settings = {
+				ltex = {
+					checkFrequency = "save",
+					enabled = { "markdown", "plaintex", "rst", "tex", "latex" },
+					language = "auto", -- default language
+				},
+			},
+		})
+	end,
+}
