b98ef194b7525c9770bfbeb561f3787b7f81509c
thefish <thefish@zaar.be>
2026-09-08 17:52:08 +0300

    update telescope, rid off 0.1.x version

--- lua/thefish/plugins/mason-lspconfig.lua
+++ lua/thefish/plugins/mason-lspconfig.lua
@@ -1,8 +1,36 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+	"mason-org/mason-lspconfig.nvim",
+	opts = {},
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
+	handlers = {
+		-- 1. The default handler (configures all other servers automatically)
+		function(server_name)
+			require("lspconfig")[server_name].setup({})
+		end,
+
+		-- 2. Your custom override handler specifically for gopls
+		["gopls"] = function()
+			require("lspconfig").gopls.setup({
+				-- Force a fallback workspace root if go.mod isn't found,
+				-- ensuring gopls attaches instead of remaining completely silent.
+				root_dir = function(fname)
+					local util = require("lspconfig.util")
+					return util.root_pattern("go.mod", ".git")(fname) or util.path.dirname(fname)
+				end,
+				settings = {
+					gopls = {
+						semanticTokens = true,
+						-- Recommended Go workspace features:
+						analyses = {
+							unusedparams = true,
+						},
+						staticcheck = true,
+					},
+				},
+			})
+		end,
+	},
 }
--- lua/thefish/plugins/telescope.lua
+++ lua/thefish/plugins/telescope.lua
@@ -1,12 +1,12 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    -- tag = '0.1.*',
-    -- or                              , branch = '0.1.x',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-        defaults = {
-            path_display = { "smart" }, -- find files easier
-        }
-    }
+	"nvim-telescope/telescope.nvim",
+	-- tag = '0.1.*',
+	-- or                              , branch = '0.1.x',
+	-- branch = '0.1.x',
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		defaults = {
+			path_display = { "smart" }, -- find files easier
+		},
+	},
 }
